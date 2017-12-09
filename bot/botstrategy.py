c1ab41cd818f4c309b50e0c8870b7ea3fb21f128
mfacchinelli <michele.facchinelli@yahoo.it>
2017-12-09 22:20:00 +0100

    Revert "Profit margin"
    This reverts commit 93a3cec72e0c1e48ccef766adeeab7c4619e01ae.

--- bot/botstrategy.py
+++ bot/botstrategy.py
@@ -2,13 +2,6 @@ from botlog import BotLog
 from botindicators import BotIndicators
 from bottrade import BotTrade
 
-# Labels
-openLabel = "OPEN"
-closedLabel = "CLOSED"
-
-# Profit margin for sell
-profitMargin = 1.01
-
 class BotStrategy( object ):
 	def __init__( self ):
 		self.output = BotLog( )
@@ -25,23 +18,21 @@ class BotStrategy( object ):
 	def tick( self, candlestick ):
 		self.currentPrice = float( candlestick.priceAverage )
 		self.prices.append( self.currentPrice )
+		
 		# self.currentClose = float( candlestick['close'] )
 		# self.closes.append( self.currentClose )
-
+		
 		average = self.indicators.movingAverage( self.prices, 15 )
-		if ( average is None ):
-			average = 0
-
-		message = "Price: %.3f \tMoving Average: %.3f" % ( candlestick.priceAverage, average )
-		self.output.log( "Price: " + str( candlestick.priceAverage ) + "\tMoving Average: " +  str( average ) )
+		self.output.log( "Price: " + str( candlestick.priceAverage ) + "\tMoving Average: " + str( average ) )
 
 		self.evaluatePositions( )
-		self.profit.append( sum( self.updateTrades( ) ) )
+		self.updateOpenTrades( )
+		self.profit.append( sum( self.showPositions( ) ) )
 
 	def evaluatePositions( self ):
 		openTrades = []
 		for trade in self.trades:
-			if ( trade.status == openLabel ):
+			if ( trade.status == "OPEN" ):
 				openTrades.append( trade )
 
 		if ( len( openTrades ) < self.numSimulTrades ):
@@ -49,17 +40,16 @@ class BotStrategy( object ):
 				self.trades.append( BotTrade( self.currentPrice, self.stopLoss ) )
 
 		for trade in openTrades:
-			if ( self.currentPrice > self.indicators.movingAverage( self.prices, 15 ) and profitMargin * trade.entryPrice < self.currentPrice ):
-				trade.close( self.currentPrice )			
+			if ( self.currentPrice > self.indicators.movingAverage( self.prices, 15 ) and trade.entryPrice < self.currentPrice ):
+				trade.close( self.currentPrice )
 
-	def updateTrades( self ):
-		i = 0
+	def updateOpenTrades( self ):
+		for trade in self.trades:
+			if ( trade.status == "OPEN" ):
+				trade.tick( self.currentPrice )
+
+	def showPositions( self ):
 		profitList = []
 		for trade in self.trades:
-			if ( trade.status == openLabel ):
-				trade.tick( self.currentPrice )
-			elif ( trade.status == closedLabel ):
-				profitList.append( trade.showTrade( ) )
-				del self.trades[i]
-			i += 1
+			profitList.append( trade.showTrade( ) )
 		return profitList
